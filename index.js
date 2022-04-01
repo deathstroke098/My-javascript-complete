0090a9ca6a7a49d037aed97b4a08dae9d0c9f94b
deathstroke098 <100844999+deathstroke098@users.noreply.github.com>
2022-04-01 16:42:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// console.log("Hello World" ,4 + 9 , "Another Log" )
+// console.warn("It's not just a signal , It's a Warning");
+// console.error("This is an Error");
+
+// var num1 = 56;
+// var num2 = 66;
+// console.log( "The answer is:" , num1+num2);
+
+// var a = 50;
+// var b = 20;
+// console.log("The value of a+b is :", a+b)
+// console.log("The value of a-b is :", a-b)
+// console.log("The value of a*b is :", a*b)
+// console.log("The value of a/b is :", a/b)
+
+// var a = 50;
+// var b = 20;
+// if (a > 20){
+//     console.log('You are an adult')
+// }
